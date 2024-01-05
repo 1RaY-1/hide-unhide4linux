14848cd742327b6b608e6831726def05391f6ff2
1RaY-1 <78962948+1RaY-1@users.noreply.github.com>
2024-01-05 19:04:46 +0100

    Add option to uninstall (delete) binaries from /usr/bin

---
+++ uninstall.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e 
+target1=hide
+target2=unhide
+
+echo "Going to remove $target1 and $target2 ..."
+sleep 2s
+
+if [[ $EUID -ne 0 ]]; then
+    echo "You have to enter sudo password.."
+fi
+
+if which $target1 > /dev/null; then
+    target1=$(which hide)
+    sudo rm $target1
+else
+    echo "$target1 doesn't exist"
+fi
+
+if which $target2 > /dev/null; then
+    target2=$(which unhide)
+    sudo rm $target2
+else
+    echo "$target2 doesn't exist"
+fi
+
+echo -e "Done\n"
